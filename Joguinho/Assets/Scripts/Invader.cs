0eee27efcc0d6c080bb0b132596133d39db3c744
Luis Felipe Flores <luismartinsflores67@gmail.com>
2025-05-10 16:32:19 +0100

    Ta ai o resto

--- Joguinho/Assets/Scripts/Invader.cs
+++ Joguinho/Assets/Scripts/Invader.cs
@@ -1,40 +1,40 @@
 using UnityEngine;
 
-[RequireComponent(typeof(SpriteRenderer))]
-[RequireComponent(typeof(Rigidbody2D))]
-[RequireComponent(typeof(BoxCollider2D))]
 public class Invader : MonoBehaviour
 {
-    public Sprite[] animationSprites = new Sprite[0];
-    public float animationTime = 1f;
-    public int score = 10;
 
-    private SpriteRenderer spriteRenderer;
-    private int animationFrame;
+    public Sprite[] animationSprites;
+
+public float animationTime = 1.0f;
+
+    private SpriteRenderer _spriteRenderer;
+
+    private int _animationFrame;
+    public System.Action killed;
 
     private void Awake()
     {
-        spriteRenderer = GetComponent<SpriteRenderer>();
-        spriteRenderer.sprite = animationSprites[0];
+        _spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     private void Start()
     {
-        InvokeRepeating(nameof(AnimateSprite), animationTime, animationTime);
+        InvokeRepeating(nameof(AnimateSprite), this.animationTime, this.animationTime);
     }
-
     private void AnimateSprite()
     {
-        animationFrame++;
-
-        // Loop back to the start if the animation frame exceeds the length
-        if (animationFrame >= animationSprites.Length)
-        {
-            animationFrame = 0;
-        }
-
-        spriteRenderer.sprite = animationSprites[animationFrame];
+        _animationFrame++;  
+        if(_animationFrame >= this.animationSprites.Length)
+    {
+        _animationFrame = 0;
     }
-
-    
+        _spriteRenderer.sprite = this.animationSprites[_animationFrame];
+    }
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.gameObject.layer == LayerMask.NameToLayer("Laser")){
+            this.killed.Invoke();
+            this.gameObject.SetActive(false);
+        }
+    }
 }
